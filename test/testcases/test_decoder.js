fabe84f92f50dc34720ece9ff3fccf03549a7706
chtyim <chtyim@gmail.com>
2011-03-02 03:37:11 +0000

    - Switched to use rhinojsunit

--- test/testcases/test_decoder.js
+++ test/testcases/test_decoder.js
@@ -1,122 +1,127 @@
-importPackage(java.lang);
-importPackage(java.io);
-importPackage(java.math);
-importPackage(org.apache.avro.io);
-importClass(org.apache.commons.codec.binary.Base64);
-
 (function() {
     
-    var generateTest = function() {
-        var byteOut = new ByteArrayOutputStream();
-        var encoder = new BinaryEncoder(byteOut);
-        
-        encoder.writeDouble(Double.POSITIVE_INFINITY);
-        encoder.writeDouble(Double.NEGATIVE_INFINITY);
-        encoder.writeDouble(Double.NaN);
-        encoder.writeDouble(Double.MAX_VALUE);
-        encoder.writeDouble(Double.MIN_VALUE);
-        encoder.writeDouble(Double.MIN_NORMAL);
-        encoder.writeDouble(Double.valueOf("0.0").doubleValue());
-        encoder.writeDouble(Double.valueOf("-0.0").doubleValue());
-        encoder.writeDouble(Double.valueOf("6547.3333333").doubleValue());
-        encoder.writeDouble(Double.valueOf("-0.000452").doubleValue());
-        
-        encoder.writeFloat(Float.POSITIVE_INFINITY);
-        encoder.writeFloat(Float.NEGATIVE_INFINITY);
-        encoder.writeFloat(Float.NaN);
-        encoder.writeFloat(Float.MAX_VALUE);
-        encoder.writeFloat(Float.MIN_VALUE);
-        encoder.writeFloat(Float.MIN_NORMAL);
-        encoder.writeFloat(Float.valueOf("0.0").floatValue());
-        encoder.writeFloat(Float.valueOf("-0.0").floatValue());
-        encoder.writeFloat(Float.valueOf("1.0").floatValue());
-        encoder.writeFloat(Float.valueOf("0.25").floatValue());
-        encoder.writeFloat(Float.valueOf("-12.662").floatValue());
-        
-        encoder.writeBoolean(true);
-        encoder.writeBoolean(false);
-
-        encoder.writeInt(10);
-        encoder.writeInt(-50);
-        encoder.writeInt(234234);
-        encoder.writeInt(-346456);
-        encoder.writeInt(Integer.MAX_VALUE - 1);
-        encoder.writeInt(Integer.MIN_VALUE + 1);
-
-        encoder.writeLong(1);
-        encoder.writeLong(-2);
-        encoder.writeLong(BigInteger.valueOf(Integer.MAX_VALUE).add(BigInteger.valueOf(1)).longValue());
-        encoder.writeLong(BigInteger.valueOf(Integer.MIN_VALUE).subtract(BigInteger.valueOf(1)).longValue());
-        encoder.writeLong(BigInteger.valueOf(Long.MAX_VALUE).divide(BigInteger.valueOf(4096)).longValue());
-        encoder.writeLong(BigInteger.valueOf(Long.MIN_VALUE).divide(BigInteger.valueOf(4096)).longValue());
-        encoder.writeLong(BigInteger.valueOf(Long.MAX_VALUE).longValue());
-        encoder.writeLong(BigInteger.valueOf(Long.MIN_VALUE).longValue());
-
-        encoder.writeBytes((new java.lang.String("Hello World!")).getBytes("ASCII"));
-        encoder.writeString("¢€");
-
-        encoder.flush();
-        byteOut.close();
-        
-        return Base64.encodeBase64String(byteOut.toByteArray());
-    };
-
-
-    // Varify the decoder
-    var decoder = AVRO.Base64BinaryDecoder();
+    var Java = new JavaImporter(java.lang,
+                                java.io, 
+                                java.math, 
+                                org.apache.avro.io, 
+                                org.apache.commons.codec.binary);
     
-    decoder.feed(generateTest());
+    var encodedBase64 = "";
+
+    testCases(test,
+
+        function setUp() {
+            var byteOut = new Java.ByteArrayOutputStream();
+            var encoder = new Java.BinaryEncoder(byteOut);
+
+            encoder.writeDouble(Java.Double.POSITIVE_INFINITY);
+            encoder.writeDouble(Java.Double.NEGATIVE_INFINITY);
+            encoder.writeDouble(Java.Double.NaN);
+            encoder.writeDouble(Java.Double.MAX_VALUE);
+            encoder.writeDouble(Java.Double.MIN_VALUE);
+            encoder.writeDouble(Java.Double.MIN_NORMAL);
+            encoder.writeDouble(Java.Double.valueOf("0.0").doubleValue());
+            encoder.writeDouble(Java.Double.valueOf("-0.0").doubleValue());
+            encoder.writeDouble(Java.Double.valueOf("6547.3333333").doubleValue());
+            encoder.writeDouble(Java.Double.valueOf("-0.000452").doubleValue());
+
+            encoder.writeFloat(Java.Float.POSITIVE_INFINITY);
+            encoder.writeFloat(Java.Float.NEGATIVE_INFINITY);
+            encoder.writeFloat(Java.Float.NaN);
+            encoder.writeFloat(Java.Float.MAX_VALUE);
+            encoder.writeFloat(Java.Float.MIN_VALUE);
+            encoder.writeFloat(Java.Float.MIN_NORMAL);
+            encoder.writeFloat(Java.Float.valueOf("0.0").floatValue());
+            encoder.writeFloat(Java.Float.valueOf("-0.0").floatValue());
+            encoder.writeFloat(Java.Float.valueOf("1.0").floatValue());
+            encoder.writeFloat(Java.Float.valueOf("0.25").floatValue());
+            encoder.writeFloat(Java.Float.valueOf("-12.662").floatValue());
+
+            encoder.writeBoolean(true);
+            encoder.writeBoolean(false);
+
+            encoder.writeInt(10);
+            encoder.writeInt(-50);
+            encoder.writeInt(234234);
+            encoder.writeInt(-346456);
+            encoder.writeInt(Java.Integer.MAX_VALUE - 1);
+            encoder.writeInt(Java.Integer.MIN_VALUE + 1);
+
+            encoder.writeLong(1);
+            encoder.writeLong(-2);
+            encoder.writeLong(Java.BigInteger.valueOf(Java.Integer.MAX_VALUE).add(Java.BigInteger.valueOf(1)).longValue());
+            encoder.writeLong(Java.BigInteger.valueOf(Java.Integer.MIN_VALUE).subtract(Java.BigInteger.valueOf(1)).longValue());
+            encoder.writeLong(Java.BigInteger.valueOf(Java.Long.MAX_VALUE).divide(Java.BigInteger.valueOf(4096)).longValue());
+            encoder.writeLong(Java.BigInteger.valueOf(Java.Long.MIN_VALUE).divide(Java.BigInteger.valueOf(4096)).longValue());
+            encoder.writeLong(Java.BigInteger.valueOf(Java.Long.MAX_VALUE).longValue());
+            encoder.writeLong(Java.BigInteger.valueOf(Java.Long.MIN_VALUE).longValue());
+
+            encoder.writeBytes((Java.String("Hello World!")).getBytes("ASCII"));
+            encoder.writeString("¢€");
+
+            encoder.flush();
+            byteOut.close();
+
+            encodedBase64 = Java.Base64.encodeBase64String(byteOut.toByteArray());
+        },
+
+        function testDecoder() {
+            // Varify the decoder
+            var decoder = AVRO.Base64BinaryDecoder();
+
+            decoder.feed(encodedBase64);
+
+            assert.that(decoder.readDouble(), eq(Number.POSITIVE_INFINITY));
+            assert.that(decoder.readDouble(), eq(Number.NEGATIVE_INFINITY));
+            assert.that(isNaN(decoder.readDouble()), isTrue());
+            assert.that(decoder.readDouble(), eq(Number.MAX_VALUE));
+            assert.that(decoder.readDouble(), eq(Number.MIN_VALUE));
+            assert.that(decoder.readDouble(), eqFloat(Java.Double.MIN_NORMAL, 1e-200));
+            assert.that(decoder.readDouble(), eqFloat(0, 1e-200));
+            assert.that(decoder.readDouble(), eqFloat(0, 1e-200));
+            assert.that(decoder.readDouble(), eqFloat(Java.Double.parseDouble("6547.3333333"), 1e-200));
+            assert.that(decoder.readDouble(), eqFloat(Java.Double.parseDouble("-0.000452"), 1e-200));
+            
+            assert.that(decoder.readFloat(), eq(Number.POSITIVE_INFINITY));
+            assert.that(decoder.readFloat(), eq(Number.NEGATIVE_INFINITY));
+            assert.that(isNaN(decoder.readFloat()), isTrue());
+            assert.that(decoder.readFloat(), eqFloat(Java.Float.MAX_VALUE, 1e-200));
+            assert.that(decoder.readFloat(), eqFloat(Java.Float.MIN_VALUE, 1e-200));
+            assert.that(decoder.readFloat(), eqFloat(Java.Float.MIN_NORMAL, 1e-200));            
+            assert.that(decoder.readFloat(), eqFloat(0, 1e-200));
+            assert.that(decoder.readFloat(), eqFloat(0, 1e-200));
+            assert.that(decoder.readFloat(), eqFloat(Java.Float.parseFloat("1"), 1e-200));
+            assert.that(decoder.readFloat(), eqFloat(Java.Float.parseFloat("0.25"), 1e-200));
+            assert.that(decoder.readFloat(), eqFloat(Java.Float.parseFloat("-12.662"), 1e-200));
     
-    Asserts.assertEquals(Number.POSITIVE_INFINITY, decoder.readDouble());
-    Asserts.assertEquals(Number.NEGATIVE_INFINITY, decoder.readDouble());
-    Asserts.assertNaN(decoder.readDouble());
-    Asserts.assertEquals(Number.MAX_VALUE, decoder.readDouble());
-    Asserts.assertEquals(Number.MIN_VALUE, decoder.readDouble());    
-    Asserts.assertDeltaEquals(Double.MIN_NORMAL, decoder.readDouble(), 1e-200);
-    Asserts.assertDeltaEquals(0, decoder.readDouble(), 1e-200);
-    Asserts.assertDeltaEquals(0, decoder.readDouble(), 1e-200);
-    Asserts.assertDeltaEquals(6547.3333333, decoder.readDouble(), 1e-200);
-    Asserts.assertDeltaEquals(-0.000452, decoder.readDouble(), 1e-200);
-    
-    Asserts.assertEquals(Number.POSITIVE_INFINITY, decoder.readFloat());
-    Asserts.assertEquals(Number.NEGATIVE_INFINITY, decoder.readFloat());
-    Asserts.assertNaN(decoder.readFloat());
-    Asserts.assertDeltaEquals(Float.MAX_VALUE, decoder.readFloat(), 1e-200);
-    Asserts.assertDeltaEquals(Float.MIN_VALUE, decoder.readFloat(), 1e-200);
-    Asserts.assertDeltaEquals(Float.MIN_NORMAL, decoder.readFloat(), 1e-200);
-    Asserts.assertDeltaEquals(0, decoder.readFloat(), 1e-200);
-    Asserts.assertDeltaEquals(0, decoder.readFloat(), 1e-200);
-    Asserts.assertDeltaEquals(1, decoder.readFloat(), 1e-200);
-    Asserts.assertDeltaEquals(0.25, decoder.readFloat(), 1e-200);
-    Asserts.assertDeltaEquals(-12.662, decoder.readFloat().toPrecision(5), 1e-200);
+            assert.that(decoder.readBoolean(), isTrue());
+            assert.that(decoder.readBoolean(), isFalse());
 
-    Asserts.assertTrue(decoder.readBoolean());
-    Asserts.assertFalse(decoder.readBoolean());    
+            assert.that(decoder.readInt(), eq(10));
+            assert.that(decoder.readInt(), eq(-50));
+            assert.that(decoder.readInt(), eq(234234));
+            assert.that(decoder.readInt(), eq(-346456));
+            assert.that(decoder.readInt(), eq(2147483646));
+            assert.that(decoder.readInt(), eq(-2147483647));
 
-    Asserts.assertEquals(10, decoder.readInt());
-    Asserts.assertEquals(-50, decoder.readInt());
-    Asserts.assertEquals(234234, decoder.readInt());
-    Asserts.assertEquals(-346456, decoder.readInt());
-    Asserts.assertEquals(2147483646, decoder.readInt());
-    Asserts.assertEquals(-2147483647, decoder.readInt());
+            assert.that(decoder.readLong(), eq(1));
+            assert.that(decoder.readLong(), eq(-2));
+            assert.that(decoder.readLong(), eq(2147483648));
+            assert.that(decoder.readLong(), eq(-2147483649));
+            assert.that(decoder.readLong(), eq(2251799813685247));
+            assert.that(decoder.readLong(), eq(-2251799813685248));
+            // The max and min long get rounded off
+            assert.that(decoder.readLong(), eq(Java.Long.MAX_VALUE));
+            assert.that(decoder.readLong(), eq(Java.Long.MIN_VALUE));
 
-    Asserts.assertEquals(1, decoder.readLong());
-    Asserts.assertEquals(-2, decoder.readLong());
-    Asserts.assertEquals(2147483648, decoder.readLong());
-    Asserts.assertEquals(-2147483649, decoder.readLong());
-    Asserts.assertEquals(2251799813685247, decoder.readLong());
-    Asserts.assertEquals(-2251799813685248, decoder.readLong());
-    // The max and min long get rounded off
-    Asserts.assertEquals(9223372036854776000, decoder.readLong());
-    Asserts.assertEquals(-9223372036854776000, decoder.readLong());
+            var bytes = decoder.readBytes();
+            var str = "";
+            for (var i = 0; i < bytes.length; i++) {
+                str += String.fromCharCode(bytes[i]);
+            }
+            assert.that(str, eq("Hello World!"));
 
-    var bytes = decoder.readBytes();
-    var str = "";
-    for (var i = 0; i < bytes.length; i++) {
-        str += String.fromCharCode(bytes[i]);
-    }
-    Asserts.assertEquals("Hello World!", str);
-
-    Asserts.assertEquals("¢€", decoder.readString());
-
-})();
+            assert.that(decoder.readString(), eq("¢€"));
+        }
+    );
+}());
