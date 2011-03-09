2c53b0cadf713717c8984d774e4231d8fe387926
chtyim@gmail.com <chtyim@gmail.com@056c39df-1cda-eef4-d03c-9f3c550c6ea9>
2011-03-09 06:13:42 +0000

    - modify test case.

--- trunk/test/testcases/test_datumreader.js
+++ trunk/test/testcases/test_datumreader.js
@@ -1,5 +1,5 @@
 (function() {
-    
+
     var Java = new JavaImporter(java.lang,
                                 java.io,
                                 java.math,
@@ -16,39 +16,39 @@
                     name : "Person",
                     fields : [
                         {name : "name", type : "string"},
-                        {name : "age", type : "int"},
+                        {name : "age", type : {type : "int"}},
                         {name : "tags", type : {type : "array", items : "string"}},
                         {name : "attrs", type : [{type : "map", values : "string"}, "null"]},
                         {name : "occupation", type : ["null", {type : "enum", name : "Occupation", symbols : ["ENGINEER", "OTHER"]}]}
-                    ]                        
+                    ]
                  };
 
     testCases(test,
-    
+
         function setUp() {
             var byteOut = new Java.ByteArrayOutputStream();
-            var encoder = new Java.BinaryEncoder(byteOut);            
+            var encoder = new Java.BinaryEncoder(byteOut);
             var recordSchema = Java.Schema.parse(JSON.stringify(schema));
             var writer = new Java.GenericDatumWriter(recordSchema);
-            
+
             var record = new Java.GenericData.Record(recordSchema);
             record.put("name", "Hello");
             record.put("age", Java.Integer.valueOf("10"));
-            
+
             var tags = new Java.GenericData.Array(2, recordSchema.getField("tags").schema());
             tags.add("Tag1");
             tags.add("Tag2");
-            
+
             record.put("tags", tags);
-            
+
             var attrs = new Java.HashMap();
             attrs.put("attr1", "value1");
             attrs.put("attr2", "value2");
-            
+
             record.put("attrs", attrs);
-            
+
             record.put("occupation", new Java.GenericData.EnumSymbol("OTHER"));
-            
+
             writer.write(record, encoder);
 
             encoder.flush();
@@ -58,24 +58,24 @@
         },
 
         function testReader() {
-            var decoder = AVRO.Base64BinaryDecoder();            
+            var decoder = AVRO.Base64BinaryDecoder();
             var reader = AVRO.DatumReader(schema, decoder);
-            
+
             decoder.feed(encodedBase64);
-                        
+
             var record = reader.read();
-            
+
             assert.that(record, isOfType("object"));
             assert.that(record.name, eq("Hello"));
             assert.that(record.age, eq(10));
 
             assert.that(record.tags, isOfType("array"));
             assert.that(record.tags, containsInOrder("Tag1", "Tag2"));
-            
+
             assert.that(record.attrs, isOfType("object"));
             assert.that(record.attrs["attr1"], eq("value1"));
             assert.that(record.attrs["attr2"], eq("value2"));
-            
+
             assert.that(record.occupation, eq("OTHER"));
         }
     );
