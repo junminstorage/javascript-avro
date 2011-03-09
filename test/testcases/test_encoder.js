efa8357a38d6d23d65ad98721305cdc71e9fcb81
chtyim <chtyim@gmail.com>
2011-03-09 09:09:23 +0000

    - Implemented encoder.writeInt

--- test/testcases/test_encoder.js
+++ test/testcases/test_encoder.js
@@ -13,6 +13,33 @@
 
         function testEncoder() {
             var encoder = AVRO.Base64BinaryEncoder();
+            encoder.writeInt(10);
+            encoder.writeInt(130);
+            encoder.writeInt(16385);
+            encoder.writeInt(1048576 * 2 + 1);
+            encoder.writeInt(Java.Integer.MAX_VALUE);
+
+            encoder.writeInt(-10);
+            encoder.writeInt(-130);
+            encoder.writeInt(-16385);
+            encoder.writeInt(-1048576 * 2 - 1);
+            encoder.writeInt(Java.Integer.MIN_VALUE);
+
+
+            var decoder = AVRO.Base64BinaryDecoder();
+            decoder.feed(encoder.getEncoded(true));
+
+            println(decoder.readInt());
+            println(decoder.readInt());
+            println(decoder.readInt());
+            println(decoder.readInt());
+            println(decoder.readInt());
+
+            println(decoder.readInt());
+            println(decoder.readInt());
+            println(decoder.readInt());
+            println(decoder.readInt());
+            println(decoder.readInt());
         }
     );
 }());
